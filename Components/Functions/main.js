7ee6c833e085a5c7c735ee06ae65fbbcd5fdbe07
Marc Norris <marcnorris007@icloud.com>
2022-12-19 12:46:46 +0000

    updated css

--- Components/Functions/main.js
+++ Components/Functions/main.js
@@ -42,12 +42,104 @@
 
 
 
-function greet() {
-    console.log("Hello There");
-}
 
-const speak = function(){
-    console.log("Good Day");
-};
+// function greet() {
+//     console.log("Hello There");
+// }
 
-speak()
+
+// const speak = function(name = "marc", time = "night"){
+//     console.log(`good ${time} ${name}`);
+// };
+
+// speak()
+// speak("Willow")
+
+
+
+// const calcArea = function(radius) {
+//     return 3.14 * radius**2;
+// };
+
+// const area = calcArea(5);
+// console.log(area)
+
+
+
+
+
+// ======================================
+
+
+// Practice with functions
+
+// function doMath(num) {
+//     num = Number(num);
+//     if(num) {
+
+//         var newNumber = (num + 100) / 5;
+//         return newNumber
+//     } else {
+
+//         return false;
+//     }
+// }
+
+// let customNum = doMath(343);
+// console.log(customNum);
+
+
+
+
+// ======================================
+
+// Practice with arrow functions
+
+// const greet = function() {
+//     return "hello, world!";
+// };
+
+// const greet = () => "hello, world!";
+    
+// const result = greet();
+// console.log(result);
+
+
+
+
+// let products = [20,15,30]
+// // let tax = .18
+
+// // const bill = function(products, tax) {
+// //     let total = 0;
+// //     for (let i = 0; i < products.length; i++) {
+// //         total += products[i] + products[i] * tax;   
+// //     }
+// //     return total;
+// // };
+
+// const bill = (products, tax) => {
+//     let total = 0;
+//     for (let i = 0; i < products.length; i++) {
+//         total += products[i] + products[i] * tax;   
+//     }
+//     return total;
+// };
+
+// console.log(bill(products, 0.2))
+
+
+const name = "Marc";
+
+// Functions
+
+const greet = () => "Hello";
+
+let resultOne = greet();
+console.log(resultOne);
+
+
+// Methods
+
+let resultTwo = name.toUpperCase();
+console.log(resultTwo)
